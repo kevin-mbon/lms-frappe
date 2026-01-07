dee0b6045bdb2fbdc15546252c6dfa8087e1c0a3
igire-kevin <kevin@igirerwanda.org>
2026-01-07 12:59:48 +0200

    Initial commit

---
+++ library_management/library_management/doctype/library_member/test_library_member.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, kevin and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibraryMember(FrappeTestCase):
+	pass
